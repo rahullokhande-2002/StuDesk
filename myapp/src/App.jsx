f3c99dfd85bd4f0ad2def232253631d9a1267289
rahul <lokhanderahul2002@gmail.com>
2026-02-04 12:05:25 +0530

    setup

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,56 +1,15 @@
-import { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Dashboard from "./pages/Dashboard";
-
-function App() {
-  const [user, setUser] = useState(
-    JSON.parse(localStorage.getItem("user"))
-  );
-
-  const login = (email, password) => {
-    const storedUser = JSON.parse(localStorage.getItem("authUser"));
-
-    if (
-      storedUser &&
-      storedUser.email === email &&
-      storedUser.password === password
-    ) {
-      localStorage.setItem("user", JSON.stringify({ email }));
-      setUser({ email });
-    } else {
-      alert("Invalid credentials");
-    }
-  };
-
-  const signup = (email, password) => {
-    localStorage.setItem("authUser", JSON.stringify({ email, password }));
-    alert("Signup successful");
-  };
-
-  const logout = () => {
-    localStorage.removeItem("user");
-    setUser(null);
-  };
-
+import React from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        {!user ? (
-          <>
-            <Route path="/" element={<Login login={login} />} />
-            <Route path="/signup" element={<Signup signup={signup} />} />
-          </>
-        ) : (
-          <Route
-            path="/"
-            element={<Dashboard logout={logout} />}
-          />
-        )}
-      </Routes>
-    </BrowserRouter>
-  );
+    <>
+     <BrowserRouter>
+     
+     
+     </BrowserRouter>
+  
+    </>
+  )
 }
 
-export default App;
+export default App
